b54d1e0c49e791ecfe4bc569dfd895026820fafd
alan-dib <61232359+alan-dib@users.noreply.github.com>
2025-09-29 18:20:55 -0300

    feat: mouseover darkens cells by ten percent each time the cursor enters the cell

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 const gridContainer = document.getElementById('etch-grid');
+let isDrawing = false;
 const elements = [];
 // generate 60 divs
 for (let i = 0; i < 60; i++){
@@ -7,3 +8,39 @@ for (let i = 0; i < 60; i++){
     gridContainer.appendChild(grid)
 }; 
 
+const cells = document.querySelectorAll('.gridDefault');
+cells.forEach(cell => {
+    cell.dataset.hoverCount = 0;
+    cell.addEventListener('mouseenter', () => {
+        if (isDrawing){
+            let count = parseInt(cell.dataset.hoverCount) || 0;
+            if(count < 10){
+                count += 1;
+                cell.dataset.hoverCount = count;
+                const opacity = count * 0.1;
+                cell.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+            }
+        }
+    })
+})
+
+gridContainer.addEventListener('mousedown', () =>{
+    isDrawing = true;
+})
+gridContainer.addEventListener('mouseup', () =>{
+    isDrawing = false;
+})
+gridContainer.addEventListener('mouseleave', () =>{
+    isDrawing = false;
+})
+
+
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        cells.forEach(cell => {
+            cell.style.backgroundColor = 'lightgrey'
+        })
+    })   
+})
